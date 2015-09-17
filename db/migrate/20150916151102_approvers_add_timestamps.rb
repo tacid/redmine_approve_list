06a8dbcfdee0c15cb5710c8d31a6f67a07507c70
Pavel Tacid <tacid@tacid.kiev.ua>
2015-09-17 15:38:30 +0300

    fix migration

--- db/migrate/20150916151102_approvers_add_timestamps.rb
+++ db/migrate/20150916151102_approvers_add_timestamps.rb
@@ -1,6 +1,6 @@
 class ApproversAddTimestamps < ActiveRecord::Migration
   def change
     add_column Approver.table_name, :created_at, :datetime
-    add_column Approver.table_name, :upadted_at, :datetime
+    add_column Approver.table_name, :updated_at, :datetime
   end
 end
