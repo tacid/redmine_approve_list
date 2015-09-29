f6e4f0919418752b4c71df361f824f8be531e20d
Pavel Tacid <tacid@tacid.kiev.ua>
2015-09-29 10:13:39 +0300

    add filter queries

--- lib/redmine_approve_list/patches/issue_query_patch.rb
+++ lib/redmine_approve_list/patches/issue_query_patch.rb
@@ -15,12 +15,16 @@ module RedmineApproveList
         def initialize_available_filters_with_approve
           initialize_available_filters_without_approve
           if User.current.logged?
-            add_available_filter "approver_id",
+            add_available_filter "approver.approver_id", label: "filter_approver_in_list",
+              :type => :list, :values => [["<< #{l(:label_me)} >>", "me"]]
+            add_available_filter "approver.need_approval_by", label: "filter_need_approval_by",
+              :type => :list, :values => [["<< #{l(:label_me)} >>", "me"]]
+            add_available_filter "approver.approved_by", label: "filter_approved_by",
               :type => :list, :values => [["<< #{l(:label_me)} >>", "me"]]
           end
         end
 
-        def sql_for_approver_id_field(field, operator, value)
+        define_method "sql_for_approver.approver_id_field" do |field, operator, value|
           db_table = Approver.table_name
 
           # "me" value substitution
@@ -29,6 +33,32 @@ module RedmineApproveList
           end
           "#{Issue.table_name}.id #{ operator == '=' ? 'IN' : 'NOT IN' } (SELECT #{db_table}.approvable_id FROM #{db_table} WHERE #{db_table}.approvable_type='Issue' AND " +
             sql_for_field(field, '=', value, db_table, 'user_id') + ')'
+        end
+
+        define_method "sql_for_approver.need_approval_by_field" do |field, operator, value|
+          db_table = Approver.table_name
+
+          # "me" value substitution
+          if value.delete("me")
+            User.current.logged? ? value.push(User.current.id.to_s) : value.push("0")
+          end
+          "#{Issue.table_name}.id #{ operator == '=' ? 'IN' : 'NOT IN' } (SELECT #{db_table}.approvable_id FROM #{db_table} WHERE #{db_table}.approvable_type='Issue' AND " +
+            sql_for_field(field, '=', value, db_table, 'user_id') + ' AND ' +
+            Approver.arel_table[:is_done].eq(false).to_sql + ' AND ' +
+            "#{db_table}.'index' = ( SELECT MIN(a3.'index') FROM #{db_table} a3 WHERE a3.is_done=#{db_table}.is_done AND a3.approvable_type=#{db_table}.approvable_type AND a3.approvable_id=#{db_table}.approvable_id)" +
+            ')'
+        end
+
+        define_method "sql_for_approver.approved_by_field" do |field, operator, value|
+          db_table = Approver.table_name
+
+          # "me" value substitution
+          if value.delete("me")
+            User.current.logged? ? value.push(User.current.id.to_s) : value.push("0")
+          end
+          "#{Issue.table_name}.id #{ operator == '=' ? 'IN' : 'NOT IN' } (SELECT #{db_table}.approvable_id FROM #{db_table} WHERE #{db_table}.approvable_type='Issue' AND " +
+            sql_for_field(field, '=', value, db_table, 'user_id') + ' AND ' +
+            Approver.arel_table[:is_done].eq(true).to_sql + ')'
         end
       end
 
